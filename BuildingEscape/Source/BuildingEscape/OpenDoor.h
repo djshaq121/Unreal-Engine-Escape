3d659698079d26d95e055bf9768797d126e13b3a
Shaquille Etienne <shaquille-etienne@hotmail.com>
2016-07-02 15:37:55 +0100

    Trigger volume

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -21,6 +21,14 @@ public:
 	// Called every frame
 	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
 
-		
+private:
+    //This keeps the variable private but visiable in the unreal engine
+    UPROPERTY(VisibleAnywhere)
+    float OpenAngle = 90.0f;
+    
+    //A trigger volume, is a invisvible volume in the game world that can be used to tell code to do something 
+    UPROPERTY(EditAnywhere)
+    ATriggerVolume  *Pressureplate;
+    
 	
 };
