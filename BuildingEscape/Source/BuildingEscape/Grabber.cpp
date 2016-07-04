de85bcff358ed2298c50e9b62453e398ceef3592
Shaquille Etienne <shaquille-etienne@hotmail.com>
2016-07-04 22:07:28 +0100

    (Lecture 71) Creating a grabber cpp, a new pawn with the grabber component and adding game mode BP

---
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -0,0 +1,52 @@
+// Copyrigth Shaquille
+
+#include "BuildingEscape.h"
+#include "Grabber.h"
+#define OUT
+
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+    UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty"));
+    
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	//Get player view point this tick(Where they are and looking)
+    FVector PlayerViewPointLocation;
+    FRotator PlayerViewPointRotation;
+    GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+    OUT PlayerViewPointLocation,
+    OUT PlayerViewPointRotation
+     );
+    
+    //Log out to test
+    UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"),
+           *PlayerViewPointLocation.ToString(),
+           *PlayerViewPointRotation.ToString()
+        );
+    //Ray-Cast out to reach distance
+    
+    //see what we hit
+}
+
